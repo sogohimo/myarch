efc284d3253f2ea7bcc506c1842f0329569d5a1c
SogoHimo <48207736+sogohimo@users.noreply.github.com>
2022-08-25 19:10:27 +0500

    1

--- archuefi1.sh
+++ archuefi1.sh
@@ -6,41 +6,6 @@ setfont cyr-sun16
 echo '2.3 Синхронизация системных часов'
 timedatectl set-ntp true
 
-echo '2.4 создание разделов'
-(
-  echo o;
-
-  echo n;
-  echo;
-  echo;
-  echo;
-  echo +500M;
-
-  echo n;
-  echo;
-  echo;
-  echo;
-  echo +30G;
-
-  echo n;
-  echo;
-  echo;
-  echo;
-  echo +8G;
-
-  echo n;
-  echo p;
-  echo;
-  echo;
-  echo a;
-  echo 1;
-
-  echo w;
-) | fdisk /dev/sda
-
-echo 'Ваша разметка диска'
-fdisk -l
-
 echo '2.4.2 Форматирование дисков'
 mkfs.ext2  /dev/sda1 -L boot
 mkfs.ext4  /dev/sda2 -L root
